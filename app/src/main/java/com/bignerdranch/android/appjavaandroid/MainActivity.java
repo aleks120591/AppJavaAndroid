432d5f5a88c9cf5c5ca5837004f2d95812077586
aleks120591@gmail.com <0p8i6y4r2w>
2016-09-05 21:29:08 +0700

    111

--- app/src/main/java/com/bignerdranch/android/appjavaandroid/MainActivity.java
+++ app/src/main/java/com/bignerdranch/android/appjavaandroid/MainActivity.java
@@ -2,12 +2,32 @@ package com.bignerdranch.android.appjavaandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.support.v7.widget.Toolbar.OnMenuItemClickListener;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initToolbar();
+    }
+
+    private void initToolbar() {
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mToolbar.setTitle(R.string.app_name);
+        mToolbar.setOnMenuItemClickListener(new OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                return false;
+            }
+        });
+        mToolbar.inflateMenu(R.menu.menu);
+
     }
 }
